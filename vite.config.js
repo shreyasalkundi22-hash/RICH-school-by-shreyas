3d5611197b3fef0ee24c20bef8c772b20841ccf1
Junior_RICH <richschoolhubli@gmail.com>
2026-08-12 21:00:44 +0530

    configure_gh_pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/RICH-school-by-shreyas/',
   server: {
     port: 3000,
     open: true
